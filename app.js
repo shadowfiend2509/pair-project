6099115158c4f4dbab82de6b89f60e0d9531aa09
ericsudhartio <*ericsudhartio2509@gmail.com>
2019-09-27 11:41:22 +0700

    deploy

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 4000;
 const Userr = require('./models').User;
 const Movie = require('./Router/MovieRouter');
 const User = require('./Router/UserRouter');
